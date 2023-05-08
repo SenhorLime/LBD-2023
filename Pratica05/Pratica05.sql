b1488f8b485f2d92443ea0419fb5daee07508579
Limão <108885897+SenhorLime@users.noreply.github.com>
2023-05-07 23:05:00 -0300

    Update Pratica05.sql

--- Pratica05/Pratica05.sql
+++ Pratica05/Pratica05.sql
@@ -1,39 +1,66 @@
-CREATE table Funcionario (
+CREATE database Empresa;
+
+use Empresa;
+
+CREATE TABLE Funcionario (
   Matricula int PRIMARY KEY,
   Nome varchar(25),
   Endereco varchar(25),
   Departamento int
 );
 
-INSERT into Funcionario (Matricula, Nome, Endereco, Departamento)
-VALUES ('1001', 'João', 'Rua 5', '108');
+INSERT INTO
+  Funcionario (Matricula, Nome, Endereco, Departamento)
+VALUES
+  ('1001', 'João', 'Rua 5', '108');
 
-INSERT INTO Funcionario (Matricula, Nome, Endereco, Departamento)
-VALUES ('1003', 'Pedro', 'Rua 8', '123');
+INSERT INTO
+  Funcionario (Matricula, Nome, Endereco, Departamento)
+VALUES
+  ('1003', 'Pedro', 'Rua 8', '123');
 
-INSERT into Funcionario (Matricula, Nome, Endereco, Departamento)
-VALUES ('1004', 'Manoel', 'Rua 6', '120');
+INSERT INTO
+  Funcionario (Matricula, Nome, Endereco, Departamento)
+VALUES
+  ('1004', 'Manoel', 'Rua 6', '120');
 
-INSERt INTO Funcionario (Matricula, Nome, Endereco, Departamento)
-VALUES ('1005', 'Pedro', 'Rua 7', '123');
+INSERT INTO
+  Funcionario (Matricula, Nome, Endereco, Departamento)
+VALUES
+  ('1005', 'Pedro', 'Rua 7', '123');
 
-INSERT into Funcionario (Matricula, Nome, Endereco, Departamento)
-VALUES ('1007', 'Maria', 'Rua6', '132');
+INSERT INTO
+  Funcionario (Matricula, Nome, Endereco, Departamento)
+VALUES
+  ('1007', 'Maria', 'Rua6', '132');
 
-UPDATE Funcionario
-Set Endereco = 'Rua 727'
-WHERE Matricula = 1001;
+UPDATE
+  Funcionario
+SET
+  Endereco = 'Rua 727'
+WHERE
+  Matricula = 1001;
 
-SELEct * FROM Funcionario;
+SELECT
+  *
+FROM
+  Funcionario;
 
-CREATE table Departamento (
+CREATE TABLE Departamento (
   IdDept int PRIMARY KEY,
   Desricao TEXT,
   FOREIGN key (IdDept) REFERENCES Funcionario(Departamento)
 );
 
-SELECT * FROM Funcionario
-WHERE Departamento = 123;
+SELECT
+  *
+FROM
+  Funcionario
+WHERE
+  Departamento = 123;
 
-DROp table Funcionario;
-DRop table Departamento;
+DROP TABLE Funcionario;
+
+DROP TABLE Departamento;
+
+DROP database Empresa;
